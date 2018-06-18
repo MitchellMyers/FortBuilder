b9af417e24ece8a522c39a1c76115158659c40a8
Mitch Myers <mmyers39@gatech.edu>
2018-06-18 19:13:35 -0400

    3D object is able to be moved up, down, left, right, forward, and backward by the buttons

--- FortBuilder/FortBuilder/ViewController.swift
+++ FortBuilder/FortBuilder/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 import SceneKit
 import ARKit
 
+let kStartingPosition = SCNVector3(0, 0, -0.6)
+let kAnimationDurationMoving: TimeInterval = 0.2
+let kMovingLengthPerLoop: CGFloat = 0.05
+let kRotationRadianPerLoop: CGFloat = 0.2
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    let selectedBlock = Block();
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +33,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -37,7 +46,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
+        configuration.planeDetection = [.horizontal]
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         sceneView.session.run(configuration)
+        addInitialBlock()
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -77,4 +90,90 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func addInitialBlock() {
+        selectedBlock.loadBlock()
+        selectedBlock.position = kStartingPosition
+        selectedBlock.rotation = SCNVector4Zero
+        sceneView.scene.rootNode.addChildNode(selectedBlock)
+    }
+    
+    
+    
+    @IBAction func moveBlockLeft(_ sender: UILongPressGestureRecognizer) {
+        let x = -deltas().cos
+        let z = deltas().sin
+        moveBlock(x: x, z: z, sender: sender)
+    }
+    
+    
+    @IBAction func moveBlockRight(_ sender: UILongPressGestureRecognizer) {
+        let x = deltas().cos
+        let z = -deltas().sin
+        moveBlock(x: x, z: z, sender: sender)
+    }
+    
+    
+    @IBAction func moveBlockUp(_ sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
+        execute(action: action, sender: sender)
+    }
+    
+    @IBAction func moveBlockDown(_ sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: 0, y: -kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
+        execute(action: action, sender: sender)
+    }
+    
+    @IBAction func moveBlockForward(_ sender: UILongPressGestureRecognizer) {
+        let x = -deltas().sin
+        let z = -deltas().cos
+        moveBlock(x: x, z: z, sender: sender)
+    }
+    
+    
+    @IBAction func moveBlockBack(_ sender: UILongPressGestureRecognizer) {
+        let x = deltas().sin
+        let z = deltas().cos
+        moveBlock(x: x, z: z, sender: sender)
+    }
+    
+    private func deltas() -> (sin: CGFloat, cos: CGFloat) {
+        return (sin: kMovingLengthPerLoop * CGFloat(sin(selectedBlock.eulerAngles.y)), cos: kMovingLengthPerLoop * CGFloat(cos(selectedBlock.eulerAngles.y)))
+    }
+    
+    private func moveBlock(x: CGFloat, z: CGFloat, sender: UILongPressGestureRecognizer) {
+        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration: kAnimationDurationMoving)
+        execute(action: action, sender: sender)
+    }
+    
+    private func execute(action: SCNAction, sender: UILongPressGestureRecognizer) {
+        let loopAction = SCNAction.repeatForever(action)
+        if sender.state == .began {
+            selectedBlock.runAction(loopAction)
+        } else if sender.state == .ended {
+            selectedBlock.removeAllActions()
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Create a SceneKit plane to visualize the plane anchor using its position and extent.
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        // `SCNPlane` is vertically oriented in its local coordinate space, so
+        // rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // Make the plane visualization semitransparent to clearly show real-world placement.
+        planeNode.opacity = 0.25
+        
+        // Add the plane visualization to the ARKit-managed node so that it tracks
+        // changes in the plane anchor as plane estimation continues.
+        node.addChildNode(planeNode)
+    }
+    
 }
