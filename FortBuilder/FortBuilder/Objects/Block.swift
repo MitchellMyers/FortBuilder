3c292fda0308be21e062c512640c45cd601eba80
sam-bretz <bretzsam@gmail.com>
2018-06-20 16:41:41 -0400

    Adding Block.swift refactor

--- FortBuilder/FortBuilder/Objects/Block.swift
+++ FortBuilder/FortBuilder/Objects/Block.swift
@@ -48,82 +48,8 @@ class Block: SCNNode {
         anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) + Float(block.width / 2),  Float(kStartingPosition.y), Float(kStartingPosition.z)))
         anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) - Float(block.width / 2), Float(kStartingPosition.y), Float(kStartingPosition.z)))
         
-        
-        
-        
-        
-        
-        
         // Return array
         return anchorPointsArray
-    }
-    
-    func moveLeft(sender: UILongPressGestureRecognizer) {
-        let x = -deltas().cos
-        let z = deltas().sin
-        moveBlock(x: x, z: z, sender: sender)
-    }
-    
-    func moveRight(sender: UILongPressGestureRecognizer) {
-        let x = deltas().cos
-        let z = -deltas().sin
-        moveBlock(x: x, z: z, sender: sender)
-    }
-    
-    func moveUp(sender: UILongPressGestureRecognizer) {
-        let action = SCNAction.moveBy(x: 0, y: kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
-        execute(action: action, sender: sender)
-    }
-    
-    func moveDown(sender: UILongPressGestureRecognizer) {
-        let action = SCNAction.moveBy(x: 0, y: -kMovingLengthPerLoop, z: 0, duration: kAnimationDurationMoving)
-        execute(action: action, sender: sender)
-    }
-    
-    func moveForward(sender: UILongPressGestureRecognizer) {
-        let x = -deltas().sin
-        let z = -deltas().cos
-        moveBlock(x: x, z: z, sender: sender)
-    }
-    
-    func moveBackward(sender: UILongPressGestureRecognizer) {
-        let x = deltas().sin
-        let z = deltas().cos
-        moveBlock(x: x, z: z, sender: sender)
-    }
-    
-    func rotateLeft(sender: UITapGestureRecognizer) {
-        let action = SCNAction.rotateBy(x: 0, y: kRotationRadianHalfPi, z: 0, duration: kAnimationDurationMoving)
-        self.runAction(action)
-    }
-    
-    func rotateRight(sender: UITapGestureRecognizer) {
-        let action = SCNAction.rotateBy(x: 0, y: -kRotationRadianHalfPi, z: 0, duration: kAnimationDurationMoving)
-        self.runAction(action)
-    }
-    
-    func rotateUp(sender: UITapGestureRecognizer) {
-        let action = SCNAction.rotateBy(x: 0, y: 0, z: kRotationRadianHalfPi, duration: kAnimationDurationMoving)
-        print(self.position)
-        self.runAction(action)
-    }
-    
-    private func deltas() -> (sin: CGFloat, cos: CGFloat) {
-        return (sin: kMovingLengthPerLoop * CGFloat(sin(self.eulerAngles.y)), cos: kMovingLengthPerLoop * CGFloat(cos(self.eulerAngles.y)))
-    }
-    
-    private func moveBlock(x: CGFloat, z: CGFloat, sender: UILongPressGestureRecognizer) {
-        let action = SCNAction.moveBy(x: x, y: 0, z: z, duration: kAnimationDurationMoving)
-        execute(action: action, sender: sender)
-    }
-    
-    private func execute(action: SCNAction, sender: UILongPressGestureRecognizer) {
-        let loopAction = SCNAction.repeatForever(action)
-        if sender.state == .began {
-            self.runAction(loopAction)
-        } else if sender.state == .ended {
-            self.removeAllActions()
-        }
     }
     
 }
