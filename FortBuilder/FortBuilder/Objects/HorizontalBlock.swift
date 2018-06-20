672907f93a3199c7b8b70f2fc065c6e44c28a7d2
sam-bretz <bretzsam@gmail.com>
2018-06-20 09:30:35 -0400

    Adding Horizontal Block

---
+++ FortBuilder/FortBuilder/Objects/HorizontalBlock.swift
@@ -0,0 +1,9 @@
+//
+//  HorizontalBlock.swift
+//  FortBuilder
+//
+//  Created by Samuel Alexander Bretz on 6/20/18.
+//  Copyright © 2018 BreMy Software. All rights reserved.
+//
+
+import Foundation
