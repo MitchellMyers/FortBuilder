96e6946b2456896dacbd34f6832f6022f4c25e3e
sam-bretz <bretzsam@gmail.com>
2018-06-21 10:10:33 -0400

    sync up

--- FortBuilder/FortBuilder/Objects/Block.swift
+++ FortBuilder/FortBuilder/Objects/Block.swift
@@ -20,60 +20,23 @@ class Block: SCNNode {
         material.diffuse.contents = UIImage(named: "../art.scnassets/wooden_texture.png")
         box.materials = [material]
         
-        // get Anchor Point locations
-//        let anchorPointLocations = getAnchorPointVectors(block: box)
-        // create anchor point for each location
-//        // get Anchor Point locations
-//        let anchorPointLocations = getAnchorPointVectors(block: box)
-//        // create anchor point for each location
-//        let anchorPointBuilder = AnchorPointBuilder()
-//        for anchor in anchorPointLocations {
-//            let ap = anchorPointBuilder.getNewAnchorPoint()
-//            ap.position = anchor
-//            addChildNode(ap)
-//        }
+  
         
         let wrapperNode = SCNNode(geometry: box)
         
         addChildNode(wrapperNode)
     }
     
-//    /*
-//     Function that finds the anchor points for a block
-//     Params: block: SCNBox
-//     Returns: anchorPointsArray: Array<SCNVector3> - array of SCNVectors representing the anchor points
-//     */
-//    func getAnchorPointVectors(block: SCNBox) -> Array<SCNVector3> {
-//        // Instantiate Return Array
-//        var anchorPointsArray: Array<SCNVector3> = Array()
-//
-//        // Get anchor points
-//        anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) + Float(block.width / 2),  Float(kStartingPosition.y), Float(kStartingPosition.z)))
-//        anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) - Float(block.width / 2), Float(kStartingPosition.y), Float(kStartingPosition.z)))
-//
-//        // Return array
-//        return anchorPointsArray
-//    }
+    /*
+     Function that finds the anchor points for a block
+     Params: None
+     Returns: anchorPointsArray: Array<SCNVector3> - array of SCNVectors representing the anchor points
+     */
     func getAnchorPoints() -> Array<SCNVector3> {
         let anchors = [SCNVector3]()
         return anchors
     }
     
-    /*
-     Function that finds the anchor points for a block
-     Params: block: SCNBox
-     Returns: anchorPointsArray: Array<SCNVector3> - array of SCNVectors representing the anchor points
-     */
-    func getAnchorPointVectors(block: SCNBox) -> Array<SCNVector3> {
-        // Instantiate Return Array
-        let anchorPointsArray: Array<SCNVector3> = Array()
-        
-        // Get anchor points
-//        anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) + Float(block.width / 2),  Float(kStartingPosition.y), Float(kStartingPosition.z)))
-//        anchorPointsArray.append(SCNVector3Make(Float(kStartingPosition.x) - Float(block.width / 2), Float(kStartingPosition.y), Float(kStartingPosition.z)))
-        
-        // Return array
-        return anchorPointsArray
-    }
+    
     
 }
